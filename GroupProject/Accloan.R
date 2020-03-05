fb917456f50f233afe492e25af8f1fe156492d3d
Ayobami Adeyeye <ayobamilol@hotmail.com>
2020-03-05 12:45:22 +0100

    day
    2

--- GroupProject/Accloan.R
+++ GroupProject/Accloan.R
@@ -1,4 +1,31 @@
 library(pacman)
-pacman::p_load(data.table,tidyverse,lubridate,ggplot2, dplyr, tidyr)
+pacman::p_load(data.table,tidyverse,lubridate,ggplot2, dplyr, corrplot, cowplot)
 
-AllLoans <- fread('all_loans.csv', stringsAsFactors = F, drop = c('PRODUCT_NAME', 'CURRENCY'))
+### Importing all loans data set
+Loans <- fread('all_loans.csv', stringsAsFactors = F, drop = c('PRODUCT_NAME', 'LOAN_REF', 'CURRENCY'))
+View(Loans)
+Loanpc <- Loans %>% group_by(PRODUCT_CODE) %>% 
+  summarise(count = n()) %>% 
+  arrange(desc(count))
+view(Loanpc)
+
+Loanas <- Loans %>% group_by(ACCOUNT_STATUS) %>% 
+  summarise(count = n()) %>% 
+  arrange(desc(count))
+view(Loanas)
+
+### count by date
+Loandt <- Loans %>% group_by()
+
+plot(Loanas$ACCOUNT_STATUS, Loanas$count, xlab = 'Account',ylab = 'count')
+
+AllLoansagg <- AllLoans %>% aggregate(by = list(CUSTOMER_UNIQUE_ID = CUSTOMER_UNIQUE_ID),FUN=sum)
+
+
+AllLoansdff <- AllLoans %>% 
+  group_by(CUSTOMER_UNIQUE_ID) %>%
+  filter(BOOK_DATE == max(BOOK_DATE))
+
+AllLoansdf <- AllLoans %>% group_by(CUSTOMER_UNIQUE_ID, MASKED_ACCOUNT) %>% 
+  summarise(AMOUNT_FINANCED = sum(AMOUNT_FINANCED)) %>% 
+  summarise(AMOUNT_FINANCED = sum(AMOUNT_FINANCED))
